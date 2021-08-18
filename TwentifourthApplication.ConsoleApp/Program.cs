a8912bad6502dc187c45e08692ccd2a2e879c789
Name <email@example.ru>
2021-08-18 17:47:16 +0600

    Добавьте файлы проекта.

---
+++ TwentifourthApplication.ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace TwentifourthApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Catalog()
+        {
+            string pathToDirectory = @"C:\Users\TheDarkKnight\Downloads\Students";
+            DirectoryInfo dir = new DirectoryInfo(pathToDirectory);
+            if(!dir.Exists)
+            {
+                dir.Create();
+            }
+            
+        }
+        static void WriteValues()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(@"C:\Users\TheDarkKnight\Downloads\Students\Students.dat", FileMode.Open)))
+            {
+                
+            }
+            
+        }
+        static void ReadValues()
+        {
+            string Name;
+            string Group;
+            
+
+            
+            if(File.Exists(@"C:\Users\TheDarkKnight\Downloads\Students\Students.dat"))
+            {
+                using(BinaryReader reader = new BinaryReader(File.Open(@"C:\Users\TheDarkKnight\Downloads\Students\Students.dat", FileMode.Open)))
+                {
+                    Name = reader.ReadString();
+                    Group = reader.ReadString();
+                    
+                }
+                var filePath = @"C:\Users\TheDarkKnight\Downloads\Students\Group.txt";
+                
+                if (File.Exists(filePath))
+                {
+                    File.Create(Group);
+                    File.Create(Name);
+                    
+                }
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    
+                    sw.WriteLine(Group);
+                    sw.WriteLine(Name);
+                    
+                }
+            }
+        }
+        public static void Main()
+        {
+            WriteValues();
+            ReadValues();
+        }
+    }
+}
